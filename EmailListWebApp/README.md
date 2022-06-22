c2058294ee1cdd21b656d9e2212312cd5c26b1e3
Dallin Whiting <96590700+dkwhiting@users.noreply.github.com>
2022-06-22 11:04:33 -0600

    Delete EmailListWebApp directory

--- EmailListWebApp/README.md
+++
@@ -1,1 +0,0 @@
-This project is a simple web app designed to insert user emails into a SQL database for the purpose of adding them to a waiting list for an upcoming project.
--- EmailListWebApp/insert.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE waiting_list;
-USE waiting_list;
-
-CREATE TABLE users(
-    email VARCHAR(255) PRIMARY KEY NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
